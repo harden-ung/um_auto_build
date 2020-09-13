f8047fbdcdc26d87caf05f0bf751ebf1c6fd3786
Trung Ung <trung.ung@meetingpackage.com>
2020-09-13 21:08:52 +0300

    handle error

--- start-me-once.bat
+++ start-me-once.bat
@@ -1,2 +1,9 @@
+git update-index --assume-unchanged settings.json
+git update-index --assume-unchanged settings-example.json
+git update-index --assume-unchanged proxies.txt
+git update-index --assume-unchanged accounts.txt
+git update-index --assume-unchanged songWriterNames.txt
+git update-index --assume-unchanged genres/
+git update-index --assume-unchanged arts/
 del -f "node_modules/"
 npm install
