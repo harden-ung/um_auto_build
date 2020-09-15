d3eb3e458c670e986b8294a4912c2cb7eed32068
Trung Ung <trung.ung@meetingpackage.com>
2020-09-16 01:08:52 +0300

    add vcc option

--- start-me-once.bat
+++ start-me-once.bat
@@ -1,9 +1,18 @@
 git update-index --assume-unchanged settings.json
-git update-index --assume-unchanged settings-example.json
 git update-index --assume-unchanged proxies.txt
 git update-index --assume-unchanged accounts.txt
 git update-index --assume-unchanged songWriterNames.txt
+git update-index --assume-unchanged genres/*
+git update-index --assume-unchanged reports/*
+git update-index --assume-unchanged arts/*
+git update-index --assume-unchanged .vscode/*
 git update-index --assume-unchanged genres/
+git update-index --assume-unchanged reports/
 git update-index --assume-unchanged arts/
+git update-index --assume-unchanged .vscode/
+git update-index --assume-unchanged vcc.txt
+
+git update-index --no-assume-unchanged settings-example.json
+
 del -f "node_modules/"
 npm install
